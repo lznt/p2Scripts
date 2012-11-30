04321eda985456a056cb2bbec778bfdaf5204ccb
huxiaori <hxrsam@gmail.com>
2012-12-01 05:03:23 +0800

    add RefFiles.js and update MainMenu.js

--- MainMenu.js
+++ MainMenu.js
@@ -1,14 +1,5 @@
-//!ref: Scripts/Background.ui
-//!ref: Scripts/Scene.ui
-//!ref: Scripts/MainMenu.ui
-//!ref: Scripts/PropType.ui
-//!ref: Scripts/Element.ui
-//!ref: Scripts/Object.ui
-//!ref: Scripts/ManMade.ui
-//!ref: Scripts/Effect.ui
-//!ref: Props/Sun.txml
-//!ref: Props/Car.txml
-//!ref: Props/PalmTrees.txml
+
+engine.IncludeFile("local://RefFiles.js");
 
 
 engine.ImportExtension("qt.core");
